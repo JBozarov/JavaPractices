2fa7ed6204e113221b8d591bda67b79940f06a06
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2020-06-18 23:45:57 -0400

    Create Scanner-FindMin.java

---
+++ Scanner-FindMin.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("ENTER NUMBER NEXT LINE: ");
+        int count = Integer.parseInt(scanner.nextLine());
+
+        int[] newArr = readInputs(count);
+
+        int show = findMin(newArr);
+
+        System.out.println("MIN number is: ");
+        System.out.println(show);
+
+    }
+
+
+
+    public static int[] readInputs(int count) {
+        int[] result = new int[count];
+
+        for ( int i = 0; i<result.length; i++ ) {
+            int input = Integer.parseInt(scanner.nextLine());
+            result[i] = input;
+            System.out.println("Please enter next number: ");
+        }
+        return result;
+    }
+
+
+
+    public static int findMin(int[] arr) {
+        int min = arr[0];
+        for ( int i = 0; i<arr.length; i++ ) {
+            if(min>arr[i]){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
