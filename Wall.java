4d1b890b954ed71544579d6f03da3ca57372de78
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2020-06-23 17:47:03 -0400

    Create Wall.java

---
+++ Wall.java
@@ -0,0 +1,20 @@
+
+
+public class Wall {
+    private Double width;
+    private Double height; 
+    
+    
+    public Wall (Double width, Double height) {
+        this.width = width; 
+        this.height = heigth; 
+    }
+    
+    public static getWidth () {
+        return width; 
+    }
+    
+    public static getHeight () {
+    return height; 
+}
+}
