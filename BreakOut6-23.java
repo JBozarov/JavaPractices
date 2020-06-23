8fc415dabb844b87cb9d7e3979f0e0a3749b0983
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2020-06-23 14:05:23 -0400

    Update BreakOut6-23.java

--- BreakOut6-23.java
+++ BreakOut6-23.java
@@ -7,9 +7,9 @@ public class Main {
 	public static void main(String[] args) {
 
 		int[] nums = {23,34,5,2,12,3,4,5,7,8,9,67,4,12,33,87};
-		ArrayList<Integer> NumsList = new ArrayList<Integer>();
-    Collections.addAll(NumsList ,nums);
 
+		
+		System.out.print("Problem 1: "+"\n"); 
 		for (int i = 0; i<nums.length; i++ ) {
 				System.out.print(nums[i]+", "); 
 		}
@@ -18,6 +18,7 @@ public class Main {
 
 
 		// 2. enhanced for loop 
+		System.out.print("Problem 2: "+"\n"); 
 		for ( int num : nums ) {
 			System.out.print(num+", "); 
 		}
@@ -25,9 +26,12 @@ public class Main {
 			System.out.print("\n"); 
 
 			// 3. a forEach loop 
-
-			NumsList.forEach(num -> System.out.print(num+", ")); 
-
+			System.out.print("Problem 3: "+"\n"); 
+			ArrayList<Integer> numList = new ArrayList<Integer>();
+			for ( int n : nums ) {
+				numList.add(n); 
+			}
+			numList.forEach((n) -> System.out.print(n + ", "));
 
 	}
 }
@@ -46,3 +50,4 @@ public class Main {
 
 
 //2.) There is another javascript file here called legoTime.js The function is called legoTime().  Rewrite that function in java, and convert the if statements into a swtich statement.
+
